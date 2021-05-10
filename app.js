075803fa550edc8dfd908fdf6832998db1280e0e
dParikesit <dimasparikesitt@gmail.com>
2021-05-10 17:04:55 +0700

    feat: cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
+const cors = require('cors')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true});
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
